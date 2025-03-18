d8cb930edd7c786f2743942d9b24da0b9b6660fc
Freewind <nowindlee@gmail.com>
2025-03-18 20:45:30 +0800

    doing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react"
+    }) as any,
+  ],
 })
